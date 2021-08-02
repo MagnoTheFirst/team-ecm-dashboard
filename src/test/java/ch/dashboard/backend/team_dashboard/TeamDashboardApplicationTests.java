cfafafb715b167651b7463f9e47cc96daf2246ed
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-08-02 21:26:03 +0200

    first lady

---
+++ src/test/java/ch/dashboard/backend/team_dashboard/TeamDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.dashboard.backend.team_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
